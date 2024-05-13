a8fb01522568cf78494db2c141de63dc001f2d29
Rayton Lin <raytonlin@gmail.com>
2024-05-13 00:31:11 -0400

    Added register page

--- journal/views.py
+++ journal/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
 # Create your views here.
 def homepage(request):
     return render(request, 'journal/index.html')
 
 def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('my-login')
+    context = {'RegistrationForm': form}
     return render(request, 'journal/register.html')
 def my_login(request):
     return render(request, 'journal/my-login.html')
